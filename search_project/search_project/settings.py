3e24fdd65c8396f82787fa658c194f0c837d1db0
山田海渡 <yamadakaito@Mac.elecom>
2025-01-31 00:00:53 +0900

    ff4

--- search_project/search_project/settings.py
+++ search_project/search_project/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 
 
 
-STATIC_URL = '/static/',
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'linkimages'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
