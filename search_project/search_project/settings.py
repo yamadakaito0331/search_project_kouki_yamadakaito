5cab3645db5b7423454cabef5bed9dc7663aca1c
山田海渡 <yamadakaito@Mac.elecom>
2025-01-30 23:26:20 +0900

    fainalda

--- search_project/search_project/settings.py
+++ search_project/search_project/settings.py
@@ -7,7 +7,7 @@ import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, '.env'))
